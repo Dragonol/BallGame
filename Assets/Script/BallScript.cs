c8e50453775fb1153ff73a432f507392003edd46
Huy Ha <huyhnc81@gmail.com>
2020-06-21 14:05:06 +0700

    Update
    tilemap, assets, physic, script for ball and wall

---
+++ Assets/Script/BallScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public float Speed;
+    public float StablizePoint;
+    public float FrictionPoint;
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 acelebration;
+        float alt = Mathf.Abs(Input.acceleration.x) + Mathf.Abs(Input.acceleration.y);
+        if (alt > 1)
+        {
+            acelebration = new Vector2(Input.acceleration.x / alt,
+                                       Input.acceleration.y / alt);
+        }
+        else
+        {
+            acelebration = Input.acceleration;
+        }
+        Vector2 filtedAccelebration = new Vector2(
+            Mathf.Abs(acelebration.x) < StablizePoint ? 0 : acelebration.x,
+            Mathf.Abs(acelebration.y) < StablizePoint ? 0 : acelebration.y);
+        rb2d.velocity += Speed * filtedAccelebration * Time.deltaTime;
+        Vector2 updateVelocity;
+        if (rb2d.velocity.x < 0)
+        {
+            updateVelocity.x = Mathf.Min(0, rb2d.velocity.x + FrictionPoint * Time.deltaTime);
+        }
+        else
+        {
+            updateVelocity.x = Mathf.Max(0, rb2d.velocity.x - FrictionPoint * Time.deltaTime);
+        }
+
+        if (rb2d.velocity.y < 0)
+        {
+            updateVelocity.y = Mathf.Min(0, rb2d.velocity.y + FrictionPoint * Time.deltaTime);
+        }
+        else
+        {
+            updateVelocity.y = Mathf.Max(0, rb2d.velocity.y - FrictionPoint * Time.deltaTime);
+        }
+        rb2d.velocity = updateVelocity;
+    }
+}
---
+++ Assets/Script/NodeScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeScript : MonoBehaviour
+{
+    public float PushForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("hello");
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject player = collision.gameObject;
+            Rigidbody2D pRb2d = player.GetComponent<Rigidbody2D>();
+            Vector2 currDir = pRb2d.velocity;
+            Vector2 contactNormal = pRb2d.transform.position - transform.position;
+            Debug.Log("Normal: x: " + contactNormal.x + " y: " + contactNormal.y);
+            //foreach (var contactPoint in collision.GetContacts()
+            //{
+            //    if(contactPoint.collider.tag == "Player")
+            //    {
+            //        contactNormal = contactPoint.normal;
+            //        Debug.Log("Normal: x: " + contactNormal.x + " y: " + contactNormal.y);
+            //        break;
+            //    }
+            //}
+
+            Debug.Log("Velocity: x: " + currDir.x + " y: " + currDir.y);
+            Vector2 newDir = Vector2.Reflect(currDir, contactNormal);
+            Debug.Log("New Dir: x: " + newDir.x + " y: " + newDir.y);
+            pRb2d.AddForce(newDir.normalized * PushForce);
+        }
+    }
+}
