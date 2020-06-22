52dafd70a14a79d03da2be3446f4b2a670d16f3e
Dragonol <huyhnc81@gmail.com>
2020-06-22 14:59:33 +0700

    fix bug

--- Assets/Script/NodeScript.cs
+++ Assets/Script/NodeScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class NodeScript : MonoBehaviour
 {
     public float PushForce;
+    public Vector2 Direction;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,23 +25,14 @@ public class NodeScript : MonoBehaviour
         {
             GameObject player = collision.gameObject;
             Rigidbody2D pRb2d = player.GetComponent<Rigidbody2D>();
-            Vector2 currDir = pRb2d.velocity;
-            Vector2 contactNormal = pRb2d.transform.position - transform.position;
-            Debug.Log("Normal: x: " + contactNormal.x + " y: " + contactNormal.y);
-            //foreach (var contactPoint in collision.GetContacts()
-            //{
-            //    if(contactPoint.collider.tag == "Player")
-            //    {
-            //        contactNormal = contactPoint.normal;
-            //        Debug.Log("Normal: x: " + contactNormal.x + " y: " + contactNormal.y);
-            //        break;
-            //    }
-            //}
-
-            Debug.Log("Velocity: x: " + currDir.x + " y: " + currDir.y);
-            Vector2 newDir = Vector2.Reflect(currDir, contactNormal);
-            Debug.Log("New Dir: x: " + newDir.x + " y: " + newDir.y);
-            pRb2d.AddForce(newDir.normalized * PushForce);
+            //Vector2 currDir = pRb2d.velocity;
+            //Vector2 contactNormal = pRb2d.transform.position - transform.position;
+            //Debug.Log("Normal: x: " + contactNormal.x + " y: " + contactNormal.y);
+            //Debug.Log("Velocity: x: " + currDir.x + " y: " + currDir.y);
+            //Vector2 newDir = Vector2.Reflect(currDir, contactNormal);
+            //Debug.Log("New Dir: x: " + newDir.x + " y: " + newDir.y);
+            //pRb2d.AddForce(newDir.normalized * PushForce);
+            pRb2d.AddForce(Direction * PushForce);
         }
     }
 }
